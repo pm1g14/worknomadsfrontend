5c23fa64134adcfbee4f0f7a52c8b3a19f920e48
Aris Fanaras <arisfan87@gmail.com>
2021-09-04 18:49:09 +0100

    Sync contracts index.

--- AdminDashboard.Wasm/Models/Contract/Contract.cs
+++ AdminDashboard.Wasm/Models/Contract/Contract.cs
@@ -6,12 +6,14 @@
         public string Address { get; set; }
         public string EmployeeName { get; set; }
         public string EmployeeSurname { get; set; }
+        public string BusinessPartner { get; set; }
         public string EmployeeWalletAddress { get; set; }
         public string CompanyWalletAddress { get; set; }
         public string PhoneNumber { get; set; }
         public string CountryOfResidence { get; set; }
         public string Email { get; set; }
         public decimal RemainingBalance { get; set; }
+        public bool Active { get; set; }
         public ContractDetails ContractDetails { get; set; } = new();
     }
 }
---
+++ AdminDashboard.Wasm/Models/Contract/ContractIndex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Wasm.Models.Contract
+{
+    public class ContractIndex
+    {
+        public List<Contract> Contracts { get; set; }
+    }
+}
